4aaad7e7ea3170a49e40c989e3b7ad2d6041d8d3
Alejandro Cornejo <alcornejo@ebay.com>
2016-04-19 16:25:16 +0200

    simplifying the right command conditional

--- src/Rover.java
+++ src/Rover.java
@@ -28,29 +28,13 @@ public class Rover {
             } else if (command.equals("r")) {
                 // Rotate Rover to the right
                 if (direction.equals("N")) {
-                    if (command.equals("r")) {
-                        direction = "E";
-                    } else {
-                        direction = "W";
-                    }
+                    direction = "E";
                 } else if (direction.equals("S")) {
-                    if (command.equals("r")) {
                         direction = "W";
-                    } else {
-                        direction = "E";
-                    }
                 } else if (direction.equals("W")) {
-                    if (command.equals("r")) {
                         direction = "N";
-                    } else {
-                        direction = "S";
-                    }
                 } else {
-                    if (command.equals("r")) {
                         direction = "S";
-                    } else {
-                        direction = "N";
-                    }
                 }
             } else {
 
