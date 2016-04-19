c405da46d9ebef55c49bccdfc61d17c4ec9d5878
Alejandro Cornejo <alcornejo@ebay.com>
2016-04-19 16:23:10 +0200

    breaking the or conditional between directions to be able to see symmetry

--- src/Rover.java
+++ src/Rover.java
@@ -14,8 +14,35 @@ public class Rover {
         for (int i = 0; i < commandsSequence.length(); ++i) {
             String command = commandsSequence.substring(i, i + 1);
 
-            if (command.equals("l") || command.equals("r")) {
+            if (command.equals("l")) {
 
+                // Rotate Rover
+                if (direction.equals("N")) {
+                    if (command.equals("r")) {
+                        direction = "E";
+                    } else {
+                        direction = "W";
+                    }
+                } else if (direction.equals("S")) {
+                    if (command.equals("r")) {
+                        direction = "W";
+                    } else {
+                        direction = "E";
+                    }
+                } else if (direction.equals("W")) {
+                    if (command.equals("r")) {
+                        direction = "N";
+                    } else {
+                        direction = "S";
+                    }
+                } else {
+                    if (command.equals("r")) {
+                        direction = "S";
+                    } else {
+                        direction = "N";
+                    }
+                }
+            } else if (command.equals("r")) {
                 // Rotate Rover
                 if (direction.equals("N")) {
                     if (command.equals("r")) {
